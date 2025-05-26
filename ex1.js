143e487259e908bd5bd35ebf4f5ff16c817936f9
승민 이 <rlaxodud5877@gmail.com>
2025-05-26 02:21:25 +0000

    0526

---
+++ ex1.js
@@ -0,0 +1,16 @@
+// 1. 변수 선언
+let name = '민수';      // 값 변경 가능
+const age = 20;         // 값 변경 불가
+
+// 2. 변수 출력
+console.log(`안녕하세요, 제 이름은 ${name}이고, 나이는 ${age}살입니다.`);
+
+// 3. 값 변경 (let 으로 선언한 변수만!)
+name = '철수';
+console.log(`이제 제 이름은 ${name}입니다.`);
+
+// 4. 간단한 계산 예시
+let a = 5;
+let b = 3;
+let sum = a + b;
+console.log(`${a} + ${b} = ${sum}`);
